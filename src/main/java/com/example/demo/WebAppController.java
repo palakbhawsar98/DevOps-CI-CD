50d9595ad689ad3a7a115792584b24f47084cf49
Palak Bhawsar <palakbhawsar5@gmail.com>
2023-02-12 13:12:01 +0530

    Pushed Maven project

---
+++ src/main/java/com/example/demo/WebAppController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebAppController {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "<h1 style=\"color: hotpink; font-size: 40px; font: cursive;\">Hello Reader, Thanks for Completing this Project 💻 </h1>";
+	} 
+}
